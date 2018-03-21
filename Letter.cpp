65d475bc88279c16f53d715bdfe5ae7fe8c5876e
Noueman Khalikine <noueman.khal@gmail.com>
2018-03-21 17:20:40 +0000

    fixing structure

--- Letter.cpp
+++ Letter.cpp
@@ -1,26 +1,17 @@
-#ifndef LETTER_H_
-#define LETTER_H_
-
 #include <string>
 
 using namespace std;
 
-class Letter {
-public:
-	Letter(char* adc);
+Letter::position = 0;
 
-	string getLine(int n){
-		return lines[n];
-	}
+string Letter::getLine(int n){
+	return lines[n];
+}
 
-	void setLine( string line , int n){
-		if(n==position) position++;
-		lines[n]=line;
-	}
-private:
-	string lines[5];
-	int position=0;
+void Letter::setLine(string line , int n){
+	if(n==position) position++;
+	lines[n]=line;
+}
 
-};
 
-#endif /* LETTER_H_ */
+
