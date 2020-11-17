3bd6bdf0a34ea9b8e7b5206cd67b75f5dd754ef3
NouemanKHAL <noueman.khal@gmail.com>
2020-11-17 18:04:34 +0100

    The program takes a filename as input, and outputs the result to a file

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <fstream>
 #include "Character.hpp"
 #include "TextTransformer.hpp"
 
 int main(int argc, char *argv[])
 {
-    std::string input;
-    std::getline(std::cin, input);
-    std::cout << TextTransformer::getInstance().get(input.begin(), input.end()) << std::endl;
+    std::ifstream inputFile(argv[1]);
+
+    if (inputFile) {
+        std::stringstream buf;
+        buf << inputFile.rdbuf();
+
+        std::string inputFileToStr = buf.str();
+
+        std::string filename = argv[1];
+        filename = filename.substr(0, std::max(0, static_cast<int>(filename.size()) - 4)) + "-geek.txt";
+
+
+        std::string result = TextTransformer::getInstance().get(inputFileToStr.begin(), inputFileToStr.end());
+
+        std::ofstream outputFile(filename, std::ios::out);
+        outputFile << result;
+    } else {
+        std::cout << "File Not Found : " << argv[1] << " ." << std::endl;
+    }
 }
